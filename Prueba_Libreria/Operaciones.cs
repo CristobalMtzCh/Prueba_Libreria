2d7441be61edfde29cc55b99d8c69412718a7d8a
cristobal chavarria <cris_chavarriax98@outlook.com>
2020-10-19 18:06:26 -0700

    Guardar Inf Libreria

---
+++ Prueba_Libreria/Operaciones.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba_Libreria
+{
+    public class Operaciones
+    {
+        public static double suma(double num1, double num2) {
+            return num1 + num2; 
+        }
+    }
+}
